1d0d57afd16f6185868be2f477260316258aaec5
Agung Pratama <apratama@traveloka.com>
2020-02-21 18:51:43 +0700

    Able to do js to go interop

--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/bangau1/ezimage"
+	"syscall/js"
 )
+
+func test(this js.Value, args []js.Value) interface{} {
+	fmt.Println("test() is called.")
+	return nil
+}
+func registerCallback(){
+	js.Global().Set("test", js.FuncOf(test))
+}
+
+var c chan bool
+func init()  {
+	fmt.Println("Init is called")
+	c = make(chan bool)
+}
 
 func main() {
 	fmt.Println("Hello, WebAssembly!")
+	registerCallback()
+	//wait until finish
+	<-c
 }
