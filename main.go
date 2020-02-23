0e2a8de2eff6f57103700e6bf8b3331431c7f23e
Agung Pratama <apratama@traveloka.com>
2020-02-23 17:22:09 +0700

    Finalize happy path

--- main.go
+++ main.go
@@ -1,16 +1,65 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"syscall/js"
 	ezimage "github.com/bangau1/ezimage/pkg/image"
 )
 
-func addWatermark(this js.value, args[]js.Value) interface{}{
-	fmt.Println("Trying to add watermark...")
+func addWatermark(this js.Value, args[]js.Value) interface{}{
+	fmt.Println("Trying to add watermark...%T", args[0].Type())
+	fmt.Println("type baseImage %T", args[0].Get("baseImage").Type())
+	fmt.Println("type watermarkImage %T", args[0].Get("watermarkImage").Type())
+	fmt.Println("type watermarkImage %T", args[0].Get("w").Type())
+	baseImageJs := args[0].Get("baseImage")
+	watermarkImageJs :=  args[0].Get("watermarkImage")
+	baseImg := make([]byte, baseImageJs.Length())
 
-	js.Global().Get()
-	return nil
+
+	n := js.CopyBytesToGo(baseImg, baseImageJs)
+	fmt.Printf("n is %d\n", n)
+	fmt.Printf("baseImg len: %d, cap: %d\n", len(baseImg), cap(baseImg))
+
+	waterImg := make([]byte, watermarkImageJs.Length())
+	n = js.CopyBytesToGo(waterImg, watermarkImageJs)
+	fmt.Printf("n is %d\n", n)
+	fmt.Printf("waterImg len: %d, cap: %d\n", len(waterImg), cap(waterImg))
+
+	result := make(map[string]interface{})
+
+	baseImgData, err := ezimage.NewImageFromReader(bytes.NewReader(baseImg))
+	if err != nil{
+		result["error"] = err.Error()
+		return js.ValueOf(result)
+	}
+
+	watermarkImgData, err := ezimage.NewImageFromReader(bytes.NewReader(waterImg))
+	if err != nil{
+		result["error"] = err.Error()
+		return js.ValueOf(result)
+	}
+
+	resultData := ezimage.NewWaterMarkProcessing(watermarkImgData).Apply(baseImgData)
+	if resultData.Error != nil{
+		result["error"] = resultData.Error.Error()
+		return js.ValueOf(result)
+	}
+
+	buf := new(bytes.Buffer)
+	err = resultData.Data.Save(buf, ezimage.MIME_TYPE_JPEG, 90)
+	if err != nil{
+		fmt.Println("error saving data", err)
+		result["error"] = err.Error()
+		return js.ValueOf(result)
+	}
+	fmt.Println("success, now return the data")
+	dataJs := js.Global().Get("Uint8Array").New(len(buf.Bytes()))
+	fmt.Println(dataJs)
+
+	js.CopyBytesToJS(dataJs, buf.Bytes())
+	result["data"] = dataJs
+	return js.ValueOf(result)
 }
 
 func registerCallback(){
